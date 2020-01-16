4f97bcca16ddc7aff51eed3b39c27e90ae212fcd
kristin-7 <dingchuanhu@ucsb.edu>
2020-01-15 17:51:14 -0800

    KH - update contents and tests

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -11,9 +11,8 @@ public class HelloController {
         String html = "<h1>Hello World!</h1>\n" +
                       "<p>This web app is powered by \n" +
                       "<a href='https://github.com/ucsb-cs56-w20/lab02-mygithubid'>this github repo</a></p>\n";
-return html;
+        return html;
     }
 
 
-    
 }
